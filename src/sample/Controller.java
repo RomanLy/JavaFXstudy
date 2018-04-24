604451346069296e7cfbcb8d0f3ce3e3782ca1ba
Roman <lytvynenko.r.i@gmail.com>
2018-04-24 23:45:22 +0300

    implement controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+
+    public Button button;
+    public Label label;
+    public TextField input;
+
+
+    public void button(ActionEvent actionEvent) {
+        String text = input.getText();
+        label.setText(text);
+        System.out.println(text);
+    }
 }
